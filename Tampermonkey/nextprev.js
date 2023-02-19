d433c04cdb4b739148bb8a5d833762c7ba2eb9f1
KERNEL32 <72179102+Jexytd@users.noreply.github.com>
2023-02-19 20:16:58 +0700

    Bug minor fixes and improvements
    like the title says :)

--- Tampermonkey/nextprev.js
+++ Tampermonkey/nextprev.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         kodok
 // @namespace    https://github.com/Jexytd
-// @version      0.3
+// @version      0.31
 // @description  Side project kodok
 // @author       Oyen
 // @match        https://komikcast.site/chapter/*
@@ -14,11 +14,15 @@
 (function() {
     'use strict';
 
-    var next,prev,DIVs;
-    (function() {
-        DIVs = document.getElementsByClassName('nextprev')[0];
-        if (DIVs !== undefined) {
-            for (const element of DIVs.children) {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    var next,prev;
+    (async function() {
+        const DIVs_1 = await document.getElementsByClassName('nextprev')[0];
+        if (DIVs_1) {
+            for (const element of DIVs_1.children) {
                 if (!element.hasAttribute('rel')) { return 0 };
 
                 if (element.getAttribute('rel') == 'next') {
@@ -31,15 +35,17 @@
             }
         }
 
-        DIVs = document.getElementsByClassName('nav-links')[0];
-        for (const element of DIVs.children) {
-            if (element.classList.contains('nav-previous')) {
-                prev = (element.children.length == 1 && element.children[0]);
-            };
+        const DIVs_2 = await document.getElementsByClassName('nav-links')[0];
+        if (DIVs_2) {
+            for (const element of DIVs_2.children) {
+                if (element.classList.contains('nav-previous')) {
+                    prev = (element.children.length == 1 && element.children[0]);
+                };
 
-            if (element.classList.contains('nav-next')) {
-                next = (element.children.length == 1 && element.children[0]);
-            };
+                if (element.classList.contains('nav-next')) {
+                    next = (element.children.length == 1 && element.children[0]);
+                };
+            }
         }
     })()
 
@@ -55,10 +61,15 @@
         }
     }
 
-    document.querySelector('body').onmousedown = function(e) {
+    document.querySelector('body').onmousedown = async function(e) {
         if (e.isTrusted && e.cancelable) {
             if ((e.button == 4 || e.buttons == 16) && !e.relatedTarget) {
                 return next ? next.click() : alert('Next button aren\t found!')
+            }
+
+            if ((e.button == 3 || e.buttons == 8) && !e.relatedTarget) {
+                await sleep(150);
+                return prev ? prev.click() : alert('Previous button aren\t found!')
             }
         }
     }
